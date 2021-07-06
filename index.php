fceb92058ed37bc9551c7617c66dfd4e956ada90
karuniawanekasakti <karuniawankaka80@mail.ugm.ac.id>
2021-07-07 00:51:08 +0700

    menambah fitur jika username atau password kosong

--- index.php
+++ index.php
@@ -9,15 +9,24 @@
     <title>Login Page</title>
 </head>
 <body>
-    <form class = "box" action="process.php" method="POST">
+    <form name="form" class = "box" action="process.php" method="POST">
         <img src="51f6fb256629fc755b8870c801092942.png" alt="">
         <h1>LOGIN</h1>
         <input type="text" placeholder="Username" name="username">
         <input type="password" placeholder="Password" name="password">
-        <input type="submit" >
+        <input type="submit" onclick="return IsEmpty();">
     </form>
     <footer>
     <p class="copyright">© kranwn2015</p>
     </footer>
+    <script>
+        function IsEmpty() {
+    if (document.forms['form'].username.value === "" || document.forms['form'].password.value === "") {
+        alert("username atau password tidak boleh kosong");
+        return false;
+    }
+    return true;
+}
+    </script>
 </body>
 </html>
